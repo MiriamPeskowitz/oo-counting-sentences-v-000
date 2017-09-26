18ddb5cc2852874106bd55a89ec4e7fa9cfb52ba
Miriam Peskowitz <miriam5000@gmail.com>
2017-09-26 16:06:45 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -20,7 +20,7 @@ class String
     self.split(/\.\?\!/).delete_if { |w| w.size <2 }.size
      # delete those that aren't really sentences; How? If there's no words or elements in it?  ie, <2
 
-    
+
     # show it? count it. Tried to use lenght, looked it up, use .size
 
     binding.pry
