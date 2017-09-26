5c81e120ec9b0c4d5970e6e8067108fbbb25fb48
Miriam Peskowitz <miriam5000@gmail.com>
2017-09-26 15:51:04 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
-  def question?
 
+  def question?
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    how_many = []
+    how_many = self.split(/\.\?\!/)
+    how_many.length
+    binding.pry
   end
 end
